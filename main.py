7854fb39ca93acb8c4c293ec69f3cb6012880c2f
Addika1630 <134506954+Addika1630@users.noreply.github.com>
2024-10-12 05:05:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -30,19 +30,93 @@ except Exception as e:
 
 # 2 Points
 def get_transcript(node):
-     #TODO Implement the logic to fetch the transcript
-    transcript = metta.run()
-    return transcript     
+    
+    """
+    Fetches transcripts associated with a specified gene identifier.
+
+    Parameters:
+    node (list): A list containing the gene identifier as its first element.
+
+    The function constructs a MeTTa query to find all transcripts 
+    that are transcribed from the given gene. The query matches 
+    expressions of the form (transcribed_to gene transcript). 
+
+    Returns:
+    gene: The result of the MeTTa query, containing the associated transcripts.
+    """
+
+    gene = metta.run(f''' 
+        !(match &space 
+        (,(transcribed_to ({node[0]}) $transcript)) 
+           (,(transcribed_to ({node[0]}) $transcript))) 
+         
+        ''') 
+    return gene       
 
 #2 Points
 def get_protein(node):
-    #TODO Implement the logic to fetch the protein
-    protein = metta.run() 
-    return protein
+    """
+    Fetches proteins associated with a specified transcript identifier.
+
+    Parameters:
+    node (list): A list containing the gene identifier as its first element.
+
+    The function constructs a MeTTa query to find all proteins 
+    that are translated from the given transcript, following the 
+    relationship defined by (translates_to transcript protein).
+
+    Returns:
+    protein: The result of the MeTTa query, containing the associated proteins.
+    """
+
+    protein = metta.run(f''' 
+        !(match &space 
+        (, (transcribed_to ({node[0]}) $transcript) 
+        (translates_to $transcript $p) 
+            ) 
+           (, (translates_to $transcript $p))) 
+         
+        ''') 
+    return protein 
 
 #6 Points
 def metta_seralizer(metta_result):
-    #TODO Implement logic to convert the Metta output into a structured format  (e.g., a list of dictionaries) that can be easily serialized to JSON.
+    """
+    Converts the output of a MeTTa query into a structured format for easy serialization.
+
+    Parameters:
+    metta_result (list): The result of a MeTTa query, expected to contain ExpressionAtoms.
+
+    The function iterates through the provided MeTTa result, extracting edges, sources, and 
+    targets from ExpressionAtoms. It formats these values as strings, removing any surrounding 
+    parentheses, and appends them to a list of dictionaries.
+
+    Returns:
+    list: A list of dictionaries, each containing 'edge', 'source', and 'target' keys 
+    representing the relationships extracted from the MeTTa result.
+    """
+
+    metta_result = metta_result[0]  
+    result = []  
+
+    for expr in metta_result:
+        if isinstance(expr, ExpressionAtom):
+            for child in expr.get_children():
+                if isinstance(child, ExpressionAtom):
+                    edge = child.get_children()[0]  
+                    source = child.get_children()[1]  
+                    target = child.get_children()[2]  
+                    
+                    edge_value = str(edge) 
+                    source_value = str(source)  
+                    target_value = str(target)  
+
+                    result.append({
+                        'edge': edge_value.strip("()"),  
+                        'source': source_value.strip("()"),  
+                        'target': target_value.strip("()")  
+                    })
+
     return result
 
 
